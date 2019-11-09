a34bb965a2a9bd33be011af10346ec824e3cbf4c
CameronDavies01 <56071558+CameronDavies01@users.noreply.github.com>
2019-11-09 21:39:43 +0000

    Add 0.0.1 Project
    A basic version of the playable character and the waiting room are present. The playable character has the ability to move to the present locations that I want them to be able to go to.

---
+++ The Wait Times Are Hell/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,256 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    public Animator EmilyMove;
+    string Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = "1";
+        EmilyMove = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // From the back
+        if ((Input.GetButtonDown("Down")) && (Position == ("1")))
+        {
+            EmilyMove.Play("EmilyBTC");
+            StartCoroutine(AnimationBuffer2());
+        }
+
+        // From the center when facing the back
+        if ((Input.GetButtonDown("Right")) && (Position == ("2")))
+        {
+            EmilyMove.Play("EmilyTurn1");
+            StartCoroutine(AnimationBuffer3());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("2")))
+        {
+            EmilyMove.Play("EmilyCTB");
+            StartCoroutine(AnimationBuffer1());
+        }
+        if ((Input.GetButtonDown("Left")) && (Position == ("2")))
+        {
+            EmilyMove.Play("EmilyTurn2");
+            StartCoroutine(AnimationBuffer4());
+        }
+        // From the center when facing the left
+        if ((Input.GetButtonDown("Left")) && (Position == ("3")))
+        {
+            EmilyMove.Play("EmilyTurn7");
+            StartCoroutine(AnimationBuffer2());
+        }
+        if ((Input.GetButtonDown("Right")) && (Position == ("3")))
+        {
+            EmilyMove.Play("EmilyTurn8");
+            StartCoroutine(AnimationBuffer5());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("3")))
+        {
+            EmilyMove.Play("EmilyCTL");
+            StartCoroutine(AnimationBuffer7());
+        }
+        // From the center when facing the right
+        if ((Input.GetButtonDown("Left")) && (Position == ("4")))
+        {
+            EmilyMove.Play("EmilyTurn3");
+            StartCoroutine(AnimationBuffer5());
+        }
+        if ((Input.GetButtonDown("Right")) && (Position == ("4")))
+        {
+            EmilyMove.Play("EmilyTurn4");
+            StartCoroutine(AnimationBuffer2());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("4")))
+        {
+            EmilyMove.Play("EmilyCTR");
+            StartCoroutine(AnimationBuffer11());
+        }
+
+        // From the center when facing the front
+        if ((Input.GetButtonDown("Right")) && (Position == ("5")))
+        {
+            EmilyMove.Play("EmilyTurn5");
+            StartCoroutine(AnimationBuffer4());
+        }
+        if ((Input.GetButtonDown("Left")) && (Position == ("5")))
+        {
+            EmilyMove.Play("EmilyTurn6");
+            StartCoroutine(AnimationBuffer3());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("5")))
+        {
+            EmilyMove.Play("EmilyCTF");
+            StartCoroutine(AnimationBuffer6());
+        }
+
+        // From the front
+        if ((Input.GetButtonDown("Down")) && (Position == ("6")))
+        {
+            EmilyMove.Play("EmilyBTC");
+            StartCoroutine(AnimationBuffer5());
+        }
+
+        // From the left
+        if ((Input.GetButtonDown("Down")) && (Position == ("7")))
+        {
+            EmilyMove.Play("EmilyLTC");
+            StartCoroutine(AnimationBuffer3());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("7")))
+        {
+            EmilyMove.Play("EmilyLTLF");
+            StartCoroutine(AnimationBuffer8());
+        }
+        if ((Input.GetButtonDown("Left")) && (Position == ("7")))
+        {
+            EmilyMove.Play("EmilyLTW1");
+            StartCoroutine(AnimationBuffer9());
+        }
+        if ((Input.GetButtonDown("Right")) && (Position == ("7")))
+        {
+            EmilyMove.Play("EmilyLTW2");
+            StartCoroutine(AnimationBuffer10());
+        }
+
+        // From the left front
+        if ((Input.GetButtonDown("Down")) && (Position == ("8")))
+        {
+            EmilyMove.Play("EmilyLFTL");
+            StartCoroutine(AnimationBuffer7());
+        }
+
+        // From window 1
+        if ((Input.GetButtonDown("Down")) && (Position == ("9")))
+        {
+            EmilyMove.Play("EmilyW1TL");
+            StartCoroutine(AnimationBuffer7());
+        }
+
+        // From window 2
+        if ((Input.GetButtonDown("Down")) && (Position == ("10")))
+        {
+            EmilyMove.Play("EmilyW2TL");
+            StartCoroutine(AnimationBuffer7());
+        }
+
+        // From the right
+        if ((Input.GetButtonDown("Down")) && (Position == ("11")))
+        {
+            EmilyMove.Play("EmilyRTC");
+            StartCoroutine(AnimationBuffer4());
+        }
+        if ((Input.GetButtonDown("Up")) && (Position == ("11")))
+        {
+            EmilyMove.Play("EmilyRTRF");
+            StartCoroutine(AnimationBuffer12());
+        }
+        if ((Input.GetButtonDown("Left")) && (Position == ("11")))
+        {
+            EmilyMove.Play("EmilyRTW3");
+            StartCoroutine(AnimationBuffer13());
+        }
+        if ((Input.GetButtonDown("Right")) && (Position == ("11")))
+        {
+            EmilyMove.Play("EmilyRTW4");
+            StartCoroutine(AnimationBuffer14());
+        }
+
+        // From the right front
+        if ((Input.GetButtonDown("Down")) && (Position == ("12")))
+        {
+            EmilyMove.Play("EmilyRFTR");
+            StartCoroutine(AnimationBuffer11());
+        }
+
+        // From window 3
+        if ((Input.GetButtonDown("Down")) && (Position == ("13")))
+        {
+            EmilyMove.Play("EmilyW3TR");
+            StartCoroutine(AnimationBuffer11());
+        }
+
+        // From window 4
+        if ((Input.GetButtonDown("Down")) && (Position == ("14")))
+        {
+            EmilyMove.Play("EmilyW4TR");
+            StartCoroutine(AnimationBuffer11());
+        }
+
+    }
+    IEnumerator AnimationBuffer1()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("1");
+    }
+    IEnumerator AnimationBuffer2()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("2");
+    }
+    IEnumerator AnimationBuffer3()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("3");
+    }
+    IEnumerator AnimationBuffer4()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("4");
+    }
+    IEnumerator AnimationBuffer5()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("5");
+    }
+    IEnumerator AnimationBuffer6()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("6");
+    }
+    IEnumerator AnimationBuffer7()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("7");
+    }
+    IEnumerator AnimationBuffer8()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("8");
+    }
+    IEnumerator AnimationBuffer9()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("9");
+    }
+    IEnumerator AnimationBuffer10()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("10");
+    }
+    IEnumerator AnimationBuffer11()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("11");
+    }
+    IEnumerator AnimationBuffer12()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("12");
+    }
+    IEnumerator AnimationBuffer13()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("13");
+    }
+    IEnumerator AnimationBuffer14()
+    {
+        yield return new WaitForSeconds(1);
+        Position = ("14");
+    }
+}
