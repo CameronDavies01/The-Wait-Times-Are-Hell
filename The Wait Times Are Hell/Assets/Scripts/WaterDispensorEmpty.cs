a02679654dc97ee1910c18117d25c083a8cac7c0
CameronDavies01 <56071558+CameronDavies01@users.noreply.github.com>
2019-12-11 13:51:16 +0000

    Added 0.1 Project
    Added a large collection of objects that you can interact with. I also made the movement more traditional.

---
+++ The Wait Times Are Hell/Assets/Scripts/WaterDispensorEmpty.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDispensorEmpty : MonoBehaviour
+{
+    public GameObject WD1;
+    public GameObject WD2;
+    public GameObject WD3;
+    public GameObject WD4;
+    public GameObject WD5;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit RayHit;
+            Ray RayItem = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(RayItem, out RayHit));
+            {
+                if (RayHit.transform.name == "ObjectWaterDispensor1")
+                {
+                    WD2.transform.position = WD1.transform.position;
+                    WD1.SetActive(false);
+                }
+
+                if (RayHit.transform.name == "ObjectWaterDispensor2") 
+                {
+                    WD3.transform.position = WD2.transform.position;
+                    WD2.SetActive(false);
+                }
+                if (RayHit.transform.name == "ObjectWaterDispensor3")
+                {
+                    WD4.transform.position = WD3.transform.position;
+                    WD3.SetActive(false);
+                }
+                if (RayHit.transform.name == "ObjectWaterDispensor4")
+                {
+                    WD5.transform.position = WD4.transform.position;
+                    WD4.SetActive(false);
+                }
+            }
+        }
+    }
+}
